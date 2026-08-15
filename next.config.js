5dce3510631173ee1c79979e7024fd21ba70eee5
Kriti Goomer <kritigoomer@gmail.com>
2026-08-15 09:12:38 -0500

    some font changes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/',
+        source: '/', //hi
         destination: '/dashboard', 
         permanent: true,          
       },
