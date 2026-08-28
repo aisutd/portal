659902cdcf31c12d1fcf5685932290b876f58efc
PranayChintakunta <pkchin07@gmail.com>
2026-08-28 01:12:53 -0500

    add admin applications management features with API integration

---
+++ src/app/admin/applications/page.tsx
@@ -0,0 +1,5 @@
+import { AdminApplicationsShell } from "@/components/admin/admin-applications-shell";
+
+export default async function AdminApplicationsPage() {
+  return <AdminApplicationsShell />;
+}
